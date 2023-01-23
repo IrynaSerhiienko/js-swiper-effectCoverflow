29e538ed50e32e9919ac1652e258b8ee3d3df86a
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-01-23 15:20:31 +0200

    Added hero-section and several styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 const swiper = new Swiper(".cruise-slider", {
+  slidesPerView: 2.5,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
@@ -14,5 +20,9 @@ const swiper = new Swiper(".cruise-slider", {
 
   pagination: {
     el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
